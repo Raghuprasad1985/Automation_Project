a4b22a8a64ae8936f8de0113f63e021e4ab16e07
Raghuprasad1985 <57977635+Raghuprasad1985@users.noreply.github.com>
2021-06-01 11:20:44 -0700

    Update automation.sh
    Add details for Task 2 & Task 3

--- automation.sh
+++ automation.sh
@@ -3,6 +3,8 @@
 #Declaring Variables
 raghuprasadS3Bucket="upgrad-raghuprasad"
 
+raghuprasadTARFileName='Raghuprasad-httpd-logs-'$(date '+%d%m%Y-%H%M%S')'.tar'
+
 #End of declaring variables
 
 
@@ -77,3 +79,46 @@ cp ${raghuprasadTARFileName} \
 s3://${raghuprasadS3Bucket}/${raghuprasadTARFileName}
 
 #End of TAR file upload to S3 bucket
+
+#To log file upload details Inventory.html for Book Keeping
+
+fileSize=$(ls -sh $raghuprasadTARFileName | awk '{print $1}')
+
+cd /var/www/html
+pwd
+
+inventoryFile="inventory.html"
+
+if [ -e $inventoryFile ]; then
+        echo "File already exists"
+else
+        echo "Creating a file"
+        sudo touch $inventoryFile
+        sudo chmod 777 $inventoryFile -R
+        printf "%s\n"  "<b> Log Type&emsp;&emsp;Time Created&emsp;&emsp;&emsp;&emsp;Type&emsp;&emsp;Size</b> <br/><br/>" > $inventoryFile
+fi
+
+printf "%s\n"  "httpd-logs&emsp;&emsp;$(date '+%d%m%Y-%H%M%S')&emsp;&emsp;&emsp;tar&emsp;&emsp;${fileSize} <br/>" >> $inventoryFile
+
+
+#End of Book Keeping
+
+#Creation Cron Job
+
+checkForCronJob=$(sudo crontab -l || echo "")
+if [ $checkForCronJob == ""]; then
+        echo "Creating cron job"
+        cronFileName="automation.sh"
+        cd /etc/cron.d/
+        sudo touch $cronFileName
+	new_job="0 20 * * * /root/Automation_Project/automation.sh"
+        sudo chmod 777 $cronFileName -R
+        printf "$new_job" > $cronFileName
+        #echo $cronFileName | sudo crontab
+        (echo "$new_job") | sort - | uniq - | sudo  crontab -
+else
+        echo "Cron job alread exists"
+fi
+
+#End of Cron Job creation
+
